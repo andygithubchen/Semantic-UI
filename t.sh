09d2586519567b279045ab28b7c89800c2d8173a
andychen <bootoo@sina.cn>
2020-07-27 15:08:57 +0800

    fix

--- t.sh
+++ t.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+theme=$1
+
 gulp build
 
-cp -f ./dist/semantic.min.css /Golang/src/api-mom/static/css/semantic-ui/semantic.min.css
+cp -f ./dist/semantic.min.css /Golang/src/api-mom/static/css/semantic-ui/semantic.${theme}.min.css
 cp -f ./dist/semantic.min.js /Golang/src/api-mom/static/js/semantic.min.js
